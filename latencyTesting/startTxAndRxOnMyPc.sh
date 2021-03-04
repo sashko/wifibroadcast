95bc136ee103c9bfc9aa6e5cce34527fe88b12f2
consti10 <geierconstantinabc@gmail.com>
2021-03-05 00:03:08 +0100

    argh did I manage to put a bug in there ?

--- latencyTesting/startTxAndRxOnMyPc.sh
+++ latencyTesting/startTxAndRxOnMyPc.sh
@@ -4,22 +4,22 @@
 
 #MY_TX="wlp3s0"
 #MY_TX="wlxc4e984126183"
-MY_TX="wlx000f00460445" # ALFA card
+#MY_TX="wlx000f00460445" # ALFA card
 #MY_TX="wlx6cfdb9b2a156" #PW-DN421
 #MY_TX="wlx0018e7bd24db"
-#MY_TX="wlx00e0863200b9" #Taobao card
+MY_TX="wlx00e0863200b9" #Taobao card
 
-#MY_RX="wlx244bfeb71c05" #Asus card
+MY_RX="wlx244bfeb71c05" #Asus card
 #MY_RX="wlx0018e7bd24db" #Ralink card aliexpress
-MY_RX="wlxc4e9840e3cbe" #tp-link rx
+#MY_RX="wlxc4e9840e3cbe" #tp-link rx
 #MY_RX_SECONDARY="wlxc4e984126183"
 WFB_FOLDER="/home/consti10/Desktop/wifibroadcast"
 
 FEC_K=4
 FEC_N=8
 
-#MY_WIFI_CHANNEL=149
-MY_WIFI_CHANNEL=13
+MY_WIFI_CHANNEL=149
+#MY_WIFI_CHANNEL=13
 
 sudo rfkill unblock wifi
 #sudo killall ifplugd #stop management of interface
@@ -45,9 +45,9 @@ sudo iwconfig $MY_RX channel $MY_WIFI_CHANNEL
 
 
 # $WFB_FOLDER/wfb_tx -k $FEC_K -n $FEC_N -u 6000 -p 60 -M 7 -K $WFB_FOLDER/drone.key $MY_TX
-xterm -hold -e $WFB_FOLDER/wfb_tx -k $FEC_K -n $FEC_N -u 6000 -p 60 -M 4 -B 40 -K $WFB_FOLDER/drone.key -f 2 $MY_TX &
+xterm -hold -e $WFB_FOLDER/wfb_tx -k $FEC_K -n $FEC_N -u 6000 -p 60 -M 4 -B 40 -K $WFB_FOLDER/drone.key  $MY_TX &
 
-$WFB_FOLDER/wfb_rx -c 127.0.0.1 -u 6100 -p 60 -K $WFB_FOLDER/gs.key -f 10 $MY_RX
+$WFB_FOLDER/wfb_rx -c 127.0.0.1 -u 6100 -p 60 -K $WFB_FOLDER/gs.key  $MY_RX
 
 #other usefull commands:
 #sudo iw dev
