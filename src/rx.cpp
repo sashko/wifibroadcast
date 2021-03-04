8f2469a7a07310d862d7431ab9eee9d7d0d2a6a4
consti10 <geierconstantinabc@gmail.com>
2021-03-05 00:17:30 +0100

    more testing

--- src/rx.cpp
+++ src/rx.cpp
@@ -146,7 +146,7 @@ void WBReceiver::processPacket(const uint8_t WLAN_IDX, const pcap_pkthdr& hdr, c
         if(decryptedPayload == std::nullopt){
             const int blockIdx=FEC::calculateBlockIdx(wbDataHeader.nonce);
             const int fragmentIdx=FEC::calculateFragmentIdx(wbDataHeader.nonce);
-            std::cerr << "unable to decrypt packet :" << std::to_string(wbDataHeader.nonce)<<"("<<blockIdx<<":"<<fragmentIdx<<")\n";
+            std::cerr << "unable to decrypt packet :" <<"("<<blockIdx<<":"<<fragmentIdx<<")\n";
             count_p_decryption_err ++;
             return;
         }
