5e660831c04f7c8aebea0e8bc20efb7776f7b4f2
consti10 <geierconstantinabc@gmail.com>
2021-03-05 03:50:06 +0100

    formatting

--- latencyTesting/startTxAndRxOnMyPc.sh
+++ latencyTesting/startTxAndRxOnMyPc.sh
@@ -22,8 +22,8 @@ MY_RX=$TAOBAO
 WFB_FOLDER="/home/consti10/Desktop/wifibroadcast"
 #WFB_FOLDER="/home/pi/Desktop/wifibroadcast"
 
-FEC_K=4
-FEC_N=4
+FEC_K=1
+FEC_N=1
 
 MY_WIFI_CHANNEL=149
 #MY_WIFI_CHANNEL=13
