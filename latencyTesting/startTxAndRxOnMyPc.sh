0c8ae86da524e58f4a94a1df0fc55f33942ac3f3
consti10 <geierconstantinabc@gmail.com>
2021-03-05 03:49:41 +0100

    formatting

--- latencyTesting/startTxAndRxOnMyPc.sh
+++ latencyTesting/startTxAndRxOnMyPc.sh
@@ -2,21 +2,28 @@
 # Given a PC with 2 wifi cards connected that support monitor mode,
 # This starts the tx on one of them and the rx on the other one
 
+TAOBAO="wlx00e0863200b9" #Taobao card
+ASUS="wlx244bfeb71c05" #ASUS card
+
 #MY_TX="wlp3s0"
 #MY_TX="wlxc4e984126183"
 #MY_TX="wlx000f00460445" # ALFA card
 #MY_TX="wlx6cfdb9b2a156" #PW-DN421
 #MY_TX="wlx0018e7bd24db"
-MY_TX="wlx00e0863200b9" #Taobao card
-
-MY_RX="wlx244bfeb71c05" #Asus card
 #MY_RX="wlx0018e7bd24db" #Ralink card aliexpress
 #MY_RX="wlxc4e9840e3cbe" #tp-link rx
 #MY_RX_SECONDARY="wlxc4e984126183"
+
+MY_TX=$ASUS
+MY_RX=$TAOBAO
+#MY_TX="wlan0" #rpi testing
+#MY_RX="wlan1" #rpi testing
+
 WFB_FOLDER="/home/consti10/Desktop/wifibroadcast"
+#WFB_FOLDER="/home/pi/Desktop/wifibroadcast"
 
 FEC_K=4
-FEC_N=8
+FEC_N=4
 
 MY_WIFI_CHANNEL=149
 #MY_WIFI_CHANNEL=13
@@ -45,7 +52,7 @@ sudo iwconfig $MY_RX channel $MY_WIFI_CHANNEL
 
 
 # $WFB_FOLDER/wfb_tx -k $FEC_K -n $FEC_N -u 6000 -p 60 -M 7 -K $WFB_FOLDER/drone.key $MY_TX
-xterm -hold -e $WFB_FOLDER/wfb_tx -k $FEC_K -n $FEC_N -u 6000 -p 60 -M 4 -B 40 -K $WFB_FOLDER/drone.key  $MY_TX &
+xterm -hold -e $WFB_FOLDER/wfb_tx -k $FEC_K -n $FEC_N -u 6000 -p 60 -M 5 -B 20 -K $WFB_FOLDER/drone.key  $MY_TX &
 
 $WFB_FOLDER/wfb_rx -c 127.0.0.1 -u 6100 -p 60 -K $WFB_FOLDER/gs.key $MY_RX
 
