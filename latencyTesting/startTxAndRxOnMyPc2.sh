6d8f7cbd64ae08659b94e027a33bb3573be2e60d
consti10 <geierconstantinabc@gmail.com>
2021-03-18 19:52:41 +0100

    simplify test scripts

--- latencyTesting/startTxAndRxOnMyPc2.sh
+++ latencyTesting/startTxAndRxOnMyPc2.sh
@@ -28,37 +28,8 @@ FEC_PERCENTAGE=50
 RADIO_PORT=61
 
 MY_WIFI_CHANNEL=149 #5ghz channel
-#MY_WIFI_CHANNEL=13 #2.4ghz channel
 
-#sudo rfkill unblock wifi
-#sudo killall ifplugd #stop management of interface
-
-#sudo ifconfig $MY_TX down
-#sudo iw dev $MY_TX set monitor otherbss fcsfail
-#sudo ifconfig $MY_TX up
-#sudo iwconfig $MY_TX channel $MY_WIFI_CHANNEL
-#sudo iw dev $MY_TX set channel "6" HT40+
-#sudo iwconfig $MY_TX rts off
-
-#sudo ifconfig $MY_RX down
-#sudo iw dev $MY_RX set monitor otherbss fcsfail
-#sudo ifconfig $MY_RX up
-#sudo iwconfig $MY_RX channel $MY_WIFI_CHANNEL
-#sudo iwconfig $MY_RX channel "6" HT40+
-
-## test with multiple RXes
-#sudo ifconfig $MY_RX_SECONDARY down
-#sudo iw dev $MY_RX_SECONDARY set monitor otherbss fcsfail
-#sudo ifconfig $MY_RX_SECONDARY up
-#sudo iwconfig $MY_RX_SECONDARY channel 6
-
-
-# $WFB_FOLDER/wfb_tx -k $FEC_K -n $FEC_N -u 6000 -p 60 -M 7 -K $WFB_FOLDER/drone.key $MY_TX
 xterm -hold -e $WFB_FOLDER/wfb_tx -k $FEC_K -p $FEC_PERCENTAGE -u 6001 -r $RADIO_PORT -M 5 -B 20 -K $WFB_FOLDER/drone.key  $MY_TX &
 
 $WFB_FOLDER/wfb_rx -c 127.0.0.1 -u 6101 -r $RADIO_PORT -K $WFB_FOLDER/gs.key $MY_RX
 
-#other usefull commands:
-#sudo iw dev
-#nc -u localhost 6000
-#nc -u -l localhost 6100
