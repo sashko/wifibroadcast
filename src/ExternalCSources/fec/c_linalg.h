d4f1f42ee14df18eb537b8460ba5ef7aa37a7eb4
consti10 <geierconstantinabc@gmail.com>
2022-01-02 14:41:58 +0100

    test optimize mul and addmul

--- src/ExternalCSources/fec/c_linalg.h
+++ src/ExternalCSources/fec/c_linalg.h
@@ -9,13 +9,13 @@ typedef unsigned char gf;
 
 static void consti_mul(gf *dst1,const gf *src1, gf c,const int sz){
     for(int i=0;i<sz;i++){
-        dst1[i]=src1[i]*c;
+        dst1[i]=(gf)src1[i]*c;
     }
 }
 
 static void consti_addmul(gf *dst1,const gf *src1, gf c, int sz) {
     for(int i=0;i<sz;i++){
-        dst1[i]+=src1[i]*c;
+        dst1[i]+=(gf)src1[i]*c;
     }
 }
 #endif //WIFIBROADCAST_C_LINALG_H
