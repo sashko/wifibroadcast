2595560c26b93b42be799d0b8fa4bd6f66a3268c
consti10 <geierconstantinabc@gmail.com>
2022-01-04 00:05:06 +0100

    tmp test on pi

--- src/ExternalCSources/fec/libmoepgf/gf256_neon.h
+++ src/ExternalCSources/fec/libmoepgf/gf256_neon.h
@@ -15,10 +15,10 @@ xorr_neon_64(uint8_t *region1, const uint8_t *region2, size_t length)
     register uint64x1_t in, out;
 
     for (end=region1+length; region1<end; region1+=8, region2+=8) {
-        in  = vld1_u64((void *)region2);
-        out = vld1_u64((void *)region1);
+        in  = vld1_u64(region2);
+        out = vld1_u64(region1);
         out = veor_u64(in, out);
-        vst1_u64((void *)region1, out);
+        vst1_u64(region1, out);
     }
 }
 
@@ -29,10 +29,10 @@ xorr_neon_128(uint8_t *region1, const uint8_t *region2, size_t length)
     register uint64x2_t in, out;
 
     for (end=region1+length; region1<end; region1+=16, region2+=16) {
-        in  = vld1q_u64((void *)region2);
-        out = vld1q_u64((void *)region1);
+        in  = vld1q_u64(region2);
+        out = vld1q_u64(region1);
         out = veorq_u64(in, out);
-        vst1q_u64((void *)region1, out);
+        vst1q_u64(region1, out);
     }
 }
 
