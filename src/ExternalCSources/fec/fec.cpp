d46f0dccbd34015efc84f42f9f19ffdf28098fe3
consti10 <geierconstantinabc@gmail.com>
2022-01-04 00:24:21 +0100

    tmp test on pi

--- src/ExternalCSources/fec/fec.cpp
+++ src/ExternalCSources/fec/fec.cpp
@@ -359,8 +359,8 @@ static void addmul(gf *dst,const gf *src, gf c, int sz) {
     //if (c != 0) addmul1(dst, src, c, sz);
     //if (c != 0) consti_addmul(dst, src, c, sz);
     //gf256_muladd_mem(dst,c,src,sz);
-    maddrc256_flat_table(dst,src,c,sz);
-    //maddrc256_shuffle_neon_64(dst,src,c,sz);
+    //maddrc256_flat_table(dst,src,c,sz);
+    maddrc256_shuffle_neon_64(dst,src,c,sz);
 }
 
 /*
