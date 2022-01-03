b2a41f83bf76e7357438073c200ba5e4690019f7
consti10 <geierconstantinabc@gmail.com>
2022-01-04 00:11:29 +0100

    tmp test on pi

--- src/ExternalCSources/fec/fec.cpp
+++ src/ExternalCSources/fec/fec.cpp
@@ -1052,8 +1052,8 @@ test_gf()
         //slow_mul1(res1.data(),source.data(),i,X_SIZE);
         //gal_mul_region(res2.data(),source.data(),i,X_SIZE);
         slow_addmul1(res1.data(),source.data(),i,X_SIZE);
-        //maddrc256_flat_table(res2.data(),source.data(),i,X_SIZE);
-        maddrc256_shuffle_neon_64(res2.data(),source.data(),i,X_SIZE);
+        maddrc256_flat_table(res2.data(),source.data(),i,X_SIZE);
+        //maddrc256_shuffle_neon_64(res2.data(),source.data(),i,X_SIZE);
         FUCK::assertVectorsEqual(res1,res2);
     }
 
