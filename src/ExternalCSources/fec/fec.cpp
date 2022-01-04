069584e3f96889384fdac2cf8455cd7087d841a9
consti10 <geierconstantinabc@gmail.com>
2022-01-04 15:17:40 +0100

    tmp test on pi

--- src/ExternalCSources/fec/fec.cpp
+++ src/ExternalCSources/fec/fec.cpp
@@ -473,7 +473,7 @@ static void mul_consti3(gf *dst,const gf *src,gf c, int sz) {
 static inline void mul(gf *dst,const gf *src, gf c,const int sz) {
     /*fprintf(stderr, "%p = %02x * %p\n", dst, c, src);*/
     // Consti10
-    //if (c != 0) mul1(dst, src, c, sz); else memset(dst, 0, sz);
+    if (c != 0) mul1(dst, src, c, sz); else memset(dst, 0, sz);
     //if (c != 0) mul_consti2(dst, src, c, sz); else memset(dst, 0, sz);
     //gf256_mul_mem(dst,src,c,sz);
     /*const int sizeFast=sz - (sz % 8);
@@ -484,7 +484,7 @@ static inline void mul(gf *dst,const gf *src, gf c,const int sz) {
     if(sizeSlow){
         maddrc256_flat_table(&dst[sizeFast],&src[sizeFast],c,sizeSlow);
     }*/
-    mulrc256_flat_table(dst,src,c,sz);
+    //mulrc256_flat_table(dst,src,c,sz);
 }
 
 /*
