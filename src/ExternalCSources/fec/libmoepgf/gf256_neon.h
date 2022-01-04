1950970cceb128ddc0f5d690b07f4e458b2defef
consti10 <geierconstantinabc@gmail.com>
2022-01-04 21:33:51 +0100

    fix 8 vs 16 incompatibility

--- src/ExternalCSources/fec/libmoepgf/gf256_neon.h
+++ src/ExternalCSources/fec/libmoepgf/gf256_neon.h
@@ -10,9 +10,24 @@ static const uint8_t tl[MOEPGF256_SIZE][16] = MOEPGF256_SHUFFLE_LOW_TABLE;
 static const uint8_t th[MOEPGF256_SIZE][16] = MOEPGF256_SHUFFLE_HIGH_TABLE;
 
 void
-xorr_neon_128(uint8_t *region1, const uint8_t *region2, size_t length)
+xorr_neon_64(uint8_t *region1, const uint8_t *region2, size_t length)
 {
     assert(length % 8 ==0);
+    uint8_t *end;
+    register uint64x1_t in, out;
+
+    for (end=region1+length; region1<end; region1+=8, region2+=8) {
+        in  = vld1_u64((void *)region2);
+        out = vld1_u64((void *)region1);
+        out = veor_u64(in, out);
+        vst1_u64((void *)region1, out);
+    }
+}
+
+void
+xorr_neon_128(uint8_t *region1, const uint8_t *region2, size_t length)
+{
+    assert(length % 16 ==0);
     uint8_t *end;
     register uint64x2_t in, out;
 
@@ -38,7 +53,7 @@ maddrc256_shuffle_neon_64(uint8_t *region1, const uint8_t *region2,
         return;
 
     if (constant == 1) {
-        xorr_neon_128(region1, region2, length);
+        xorr_neon_64(region1, region2, length);
         return;
     }
 
