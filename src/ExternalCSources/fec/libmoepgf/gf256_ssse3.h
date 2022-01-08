4c3ecfb3f747f0ca3e71148b8d00c64f58ca657b
consti10 <geierconstantinabc@gmail.com>
2022-01-08 03:06:04 +0100

    use unaligned everywhere to be sure

--- src/ExternalCSources/fec/libmoepgf/gf256_ssse3.h
+++ src/ExternalCSources/fec/libmoepgf/gf256_ssse3.h
@@ -23,10 +23,10 @@ xorr_sse2(uint8_t *region1, const uint8_t *region2, size_t length)
     register __m128i in, out;
 
     for (end=region1+length; region1<end; region1+=16, region2+=16) {
-        in  = _mm_load_si128((const __m128i *)region2);
-        out = _mm_load_si128((const __m128i *)region1);
+        in  = _mm_loadu_si128((const __m128i *)region2);
+        out = _mm_loadu_si128((const __m128i *)region1);
         out = _mm_xor_si128(in, out);
-        _mm_store_si128((__m128i *)region1, out);
+        _mm_storeu_si128((__m128i *)region1, out);
     }
 }
 
@@ -52,8 +52,8 @@ maddrc256_shuffle_ssse3(uint8_t *region1, const uint8_t *region2,
     m2 = _mm_set1_epi8(0xf0);
 
     for (end=region1+length; region1<end; region1+=16, region2+=16) {
-        in2 = _mm_load_si128((const __m128i *)region2);
-        in1 = _mm_load_si128((const __m128i *)region1);
+        in2 = _mm_loadu_si128((const __m128i *)region2);
+        in1 = _mm_loadu_si128((const __m128i *)region1);
         l = _mm_and_si128(in2, m1);
         l = _mm_shuffle_epi8(t1, l);
         h = _mm_and_si128(in2, m2);
@@ -61,7 +61,7 @@ maddrc256_shuffle_ssse3(uint8_t *region1, const uint8_t *region2,
         h = _mm_shuffle_epi8(t2, h);
         out = _mm_xor_si128(h, l);
         out = _mm_xor_si128(out, in1);
-        _mm_store_si128((__m128i *)region1, out);
+        _mm_storeu_si128((__m128i *)region1, out);
     }
 }
 
@@ -85,14 +85,14 @@ mulrc256_shuffle_ssse3(uint8_t *region, uint8_t constant, size_t length)
     m2 = _mm_set1_epi8(0xf0);
 
     for (end=region+length; region<end; region+=16) {
-        in = _mm_load_si128((const __m128i *)region);
+        in = _mm_loadu_si128((const __m128i *)region);
         l = _mm_and_si128(in, m1);
         l = _mm_shuffle_epi8(t1, l);
         h = _mm_and_si128(in, m2);
         h = _mm_srli_epi64(h, 4);
         h = _mm_shuffle_epi8(t2, h);
         out = _mm_xor_si128(h, l);
-        _mm_store_si128((__m128i *)region, out);
+        _mm_storeu_si128((__m128i *)region, out);
     }
 }
 
