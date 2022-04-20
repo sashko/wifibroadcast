7aae2405de6bfbdde6238a43039c0f2594aadcd2
consti10 <geierconstantinabc@gmail.com>
2022-04-20 15:06:14 +0200

    refactor code such that one can use WFB TX without udp

--- src/WBLib.cmake
+++ src/WBLib.cmake
@@ -1,4 +1,5 @@
 # For including the wifibroadcast code in a cmake project
+# NOTE: Does not build the executables
 
 add_library( radiotap
         SHARED
@@ -12,8 +13,8 @@ add_library( fec
 
 add_library( wifibroadcast
         SHARED
-        ${CMAKE_CURRENT_LIST_DIR}/src/WBReceiver.cpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/WBTransmitter.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/WBReceiver.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/WBTransmitter.cpp
         )
 
 target_link_libraries( wifibroadcast radiotap fec)
