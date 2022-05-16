f8a61bc65c81ea86a10320fd88f59660efd0757a
consti10 <geierconstantinabc@gmail.com>
2022-05-16 14:35:47 +0200

    improve debugging capabilities

--- src/WBReceiver.cpp
+++ src/WBReceiver.cpp
@@ -50,7 +50,17 @@ void WBReceiver::loop() {
 }
 
 std::string WBReceiver::createDebugState()const {
-    return std::string();
+    const auto count_blocks_total=mFECDDecoder ? mFECDDecoder->count_blocks_total :0;
+    const auto count_blocks_lost=mFECDDecoder ? mFECDDecoder->count_blocks_lost :0;
+    const auto count_blocks_recovered=mFECDDecoder ? mFECDDecoder->count_blocks_recovered : 0;
+    const auto count_fragments_recovered= mFECDDecoder ? mFECDDecoder->count_fragments_recovered : 0;
+    //timestamp in ms
+    const uint64_t runTime=std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()-INIT_TIME).count();
+    std::stringstream ss;
+    ss << runTime << "\tPKT" << count_p_all << "\tRPort " << +options.radio_port << " Decryption(OK:" << count_p_decryption_ok << " Err:" << count_p_decryption_err <<
+       ") FEC(totalB:" << count_blocks_total << " lostB:" << count_blocks_lost << " recB:" << count_blocks_recovered << " recP:" << count_fragments_recovered << ")";
+
+    return ss.str();
 }
 
 void WBReceiver::dump_stats() {
