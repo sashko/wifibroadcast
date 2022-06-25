7fff28a2bca1689f7fad98d19acb712e57823a06
consti10 <geierconstantinabc@gmail.com>
2022-06-25 16:21:20 +0200

    argh if that was the issue fuck !

--- src/UDPWfibroadcastWrapper.hpp
+++ src/UDPWfibroadcastWrapper.hpp
@@ -21,11 +21,10 @@
 class UDPWBTransmitter {
  public:
   UDPWBTransmitter(RadiotapHeader radiotapHeader,
-                   const TOptions &options1,
+                   TOptions options1,
                    const std::string &client_addr,
                    int client_udp_port) {
-    wbTransmitter = std::make_unique<WBTransmitter>(
-        radiotapHeader, options1);
+    wbTransmitter = std::make_unique<WBTransmitter>(radiotapHeader, std::move(options1));
     udpReceiver = std::make_unique<SocketHelper::UDPReceiver>(client_addr,
                                                               client_udp_port,
                                                               [this](const uint8_t *payload,
@@ -59,10 +58,10 @@ class UDPWBTransmitter {
  */
 class UDPWBReceiver {
  public:
-  UDPWBReceiver(const ROptions &options1, std::string client_addr, int client_udp_port) {
+  UDPWBReceiver(ROptions options1, std::string client_addr, int client_udp_port) {
     udpMultiForwarder = std::make_unique<SocketHelper::UDPMultiForwarder>();
     addForwarder(std::move(client_addr), client_udp_port);
-    wbReceiver = std::make_unique<WBReceiver>(options1, [this](const uint8_t *payload, const std::size_t payloadSize) {
+    wbReceiver = std::make_unique<WBReceiver>(std::move(options1), [this](const uint8_t *payload, const std::size_t payloadSize) {
       onNewData(payload, payloadSize);
     });
   }
