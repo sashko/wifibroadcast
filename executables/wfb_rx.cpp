eb60038ccf712fceb456db81c891c0e459196832
consti10 <geierconstantinabc@gmail.com>
2022-12-07 03:20:13 +0100

    this is a breaking change - get rid of the custom rtp parse logic in WBTransmitter. This unneccessarily makes things harder to understand, and is a legacy from the old "always udp as input" design

--- executables/wfb_rx.cpp
+++ executables/wfb_rx.cpp
@@ -21,36 +21,36 @@ int main(int argc, char *const *argv) {
   std::string client_addr = "127.0.0.1";// default to localhost
 
   while ((opt = getopt(argc, argv, "K:c:u:r:n:k:")) != -1) {
-	switch (opt) {
-	  case 'K':options.keypair = optarg;
-		break;
-	  case 'c':client_addr = std::string(optarg);
-		break;
-	  case 'u':client_udp_port = std::stoi(optarg);
-		break;
-	  case 'r':options.radio_port = std::stoi(optarg);
-		break;
-	  case 'k':
-	  case 'n':
-		std::cout << "-n is deprecated. Please read https://github.com/Consti10/wifibroadcast/blob/master/README.md \n";
-		exit(1);
-	  default: /* '?' */
-	  show_usage:
-		fprintf(stderr,
-				"Local receiver: %s [-K rx_key] [-c client_addr] [-u udp_client_port] [-r radio_port] interface1 [interface2] ...\n",
-				argv[0]);
-		fprintf(stderr, "Default: K='%s', connect=%s:%d, radio_port=%d\n",
-				"none", client_addr.c_str(), client_udp_port, options.radio_port);
-		fprintf(stderr, "WFB version "
-						WFB_VERSION
-						"\n");
-		exit(1);
-	}
+    switch (opt) {
+      case 'K':options.keypair = optarg;
+        break;
+      case 'c':client_addr = std::string(optarg);
+        break;
+      case 'u':client_udp_port = std::stoi(optarg);
+        break;
+      case 'r':options.radio_port = std::stoi(optarg);
+        break;
+      case 'k':
+      case 'n':
+        std::cout << "-n is deprecated. Please read https://github.com/Consti10/wifibroadcast/blob/master/README.md \n";
+        exit(1);
+      default: /* '?' */
+      show_usage:
+        fprintf(stderr,
+                "Local receiver: %s [-K rx_key] [-c client_addr] [-u udp_client_port] [-r radio_port] interface1 [interface2] ...\n",
+                argv[0]);
+        fprintf(stderr, "Default: K='%s', connect=%s:%d, radio_port=%d\n",
+                "none", client_addr.c_str(), client_udp_port, options.radio_port);
+        fprintf(stderr, "WFB version "
+                WFB_VERSION
+                        "\n");
+        exit(1);
+    }
   }
   const int nRxInterfaces = argc - optind;
   if (nRxInterfaces > MAX_RX_INTERFACES) {
-	std::cout << "Too many RX interfaces " << nRxInterfaces << "\n";
-	goto show_usage;
+    std::cout << "Too many RX interfaces " << nRxInterfaces << "\n";
+    goto show_usage;
   }
   SchedulingHelper::setThreadParamsMaxRealtime();
 
@@ -58,18 +58,18 @@ int main(int argc, char *const *argv) {
 
   options.rxInterfaces.resize(nRxInterfaces);
   for (int i = 0; i < nRxInterfaces; i++) {
-	options.rxInterfaces[i] = std::string(argv[optind + i]);
+    options.rxInterfaces[i] = std::string(argv[optind + i]);
   }
   try {
-	UDPWBReceiver udpwbReceiver{options, client_addr, client_udp_port};
-	udpwbReceiver.runInBackground();
-        while (true){
-          std::cout << udpwbReceiver.createDebug();
-          std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
+    UDPWBReceiver udpwbReceiver{options, client_addr, client_udp_port};
+    udpwbReceiver.runInBackground();
+    while (true){
+      std::cout << udpwbReceiver.createDebug();
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
   } catch (std::runtime_error &e) {
-	fprintf(stderr, "Error: %s\n", e.what());
-	exit(1);
+    fprintf(stderr, "Error: %s\n", e.what());
+    exit(1);
   }
   return 0;
 }
--- executables/wfb_tx.cpp
+++ executables/wfb_tx.cpp
@@ -29,50 +29,50 @@ int main(int argc, char *const *argv) {
   std::cout << "MAX_PAYLOAD_SIZE:" << FEC_MAX_PAYLOAD_SIZE << "\n";
 
   while ((opt = getopt(argc, argv, "K:k:p:u:r:B:G:S:L:M:n:")) != -1) {
-	switch (opt) {
-          case 'K':options.keypair = optarg;
-            break;
-          case 'k':{
-            const auto fec_k=std::stoi(optarg);
-            if(fec_k>=0){
-              options.enable_fec= true;
-              options.tx_fec_options.fixed_k=fec_k;
-            }
-          }break;
-          case 'p':
-            options.tx_fec_options.overhead_percentage = std::stoi(optarg);
-            break;
-          case 'u':udp_port = std::stoi(optarg);
-            break;
-          case 'r':options.radio_port = std::stoi(optarg);
-            break;
-          case 'B':wifiParams.bandwidth = std::stoi(optarg);
-            break;
-          case 'G':wifiParams.short_gi = (optarg[0] == 's' || optarg[0] == 'S');
-            break;
-          case 'S':wifiParams.stbc = std::stoi(optarg);
-            break;
-          case 'L':wifiParams.ldpc = std::stoi(optarg);
-            break;
-          case 'M':wifiParams.mcs_index = std::stoi(optarg);
-            break;
-          case 'n':
-            std::cerr << "-n is deprecated. Please read https://github.com/Consti10/wifibroadcast/blob/master/README.md \n";
-            exit(1);
-          default: /* '?' */
-          show_usage:
-            fprintf(stderr,
-                    "Usage: %s [-K tx_key] [-k FEC_K or 0 for variable fec] [-p FEC_PERCENTAGE] [-u udp_port] [-r radio_port] [-B bandwidth] [-G guard_interval] [-S stbc] [-L ldpc] [-M mcs_index] interface \n",
-                    argv[0]);
-            fprintf(stderr, "Radio MTU: %lu\n", (unsigned long)FEC_MAX_PAYLOAD_SIZE);
-            fprintf(stderr, "WFB version "
-                    WFB_VERSION
-                            "\n");
-            exit(1);
-	}
+    switch (opt) {
+      case 'K':options.keypair = optarg;
+        break;
+      case 'k':{
+        const auto fec_k=std::stoi(optarg);
+        if(fec_k>=0){
+          options.enable_fec= true;
+          options.tx_fec_options.fixed_k=fec_k;
+        }
+      }break;
+      case 'p':
+        options.tx_fec_options.overhead_percentage = std::stoi(optarg);
+        break;
+      case 'u':udp_port = std::stoi(optarg);
+        break;
+      case 'r':options.radio_port = std::stoi(optarg);
+        break;
+      case 'B':wifiParams.bandwidth = std::stoi(optarg);
+        break;
+      case 'G':wifiParams.short_gi = (optarg[0] == 's' || optarg[0] == 'S');
+        break;
+      case 'S':wifiParams.stbc = std::stoi(optarg);
+        break;
+      case 'L':wifiParams.ldpc = std::stoi(optarg);
+        break;
+      case 'M':wifiParams.mcs_index = std::stoi(optarg);
+        break;
+      case 'n':
+        std::cerr << "-n is deprecated. Please read https://github.com/Consti10/wifibroadcast/blob/master/README.md \n";
+        exit(1);
+      default: /* '?' */
+      show_usage:
+        fprintf(stderr,
+                "Usage: %s [-K tx_key] [-k FEC_K or 0 for variable fec] [-p FEC_PERCENTAGE] [-u udp_port] [-r radio_port] [-B bandwidth] [-G guard_interval] [-S stbc] [-L ldpc] [-M mcs_index] interface \n",
+                argv[0]);
+        fprintf(stderr, "Radio MTU: %lu\n", (unsigned long)FEC_MAX_PAYLOAD_SIZE);
+        fprintf(stderr, "WFB version "
+                WFB_VERSION
+                        "\n");
+        exit(1);
+    }
   }
   if (optind >= argc) {
-	goto show_usage;
+    goto show_usage;
   }
   options.wlan = argv[optind];
 
@@ -82,15 +82,15 @@ int main(int argc, char *const *argv) {
   SchedulingHelper::setThreadParamsMaxRealtime();
 
   try {
-	UDPWBTransmitter udpwbTransmitter{wifiParams, options, SocketHelper::ADDRESS_LOCALHOST, udp_port};
-	udpwbTransmitter.runInBackground();
-        while (true){
-          std::cout << udpwbTransmitter.createDebug();
-          std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
+    UDPWBTransmitter udpwbTransmitter{wifiParams, options, SocketHelper::ADDRESS_LOCALHOST, udp_port};
+    udpwbTransmitter.runInBackground();
+    while (true){
+      std::cout << udpwbTransmitter.createDebug();
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
   } catch (std::runtime_error &e) {
-	fprintf(stderr, "Error: %s\n", e.what());
-	exit(1);
+    fprintf(stderr, "Error: %s\n", e.what());
+    exit(1);
   }
   return 0;
 }
