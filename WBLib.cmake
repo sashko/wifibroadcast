95d9b27635b3a422bdb63649b564947cad10902e
consti10 <geierconstantinabc@gmail.com>
2023-03-19 22:52:13 +0100

    properly use and only ask for SSSE3 on X86. This way we should be backwards compatible "enough"

--- WBLib.cmake
+++ WBLib.cmake
@@ -5,7 +5,7 @@
 cmake_minimum_required(VERSION 3.16.3)
 set(CMAKE_CXX_STANDARD 17)
 
-option(WB_ENABLE_SIMD_OPTIMIZATIONS "Enable NEON on ARM and AVX2 on X86 if the platform/compiler supports it" ON)
+option(WB_ENABLE_SIMD_OPTIMIZATIONS "Enable NEON on ARM and SSSE3 on X86 if the platform/compiler supports it" ON)
 #if(WIFIBROADCAST_LIBRARIES_ALREADY_BUILD)
 #if(get_property(source_list GLOBAL PROPERTY source_list_property))
 #    message(STATUS "WIFIBROADCAST_LIBRARIES_ALREADY_BUILD")
@@ -35,20 +35,20 @@ target_include_directories(wifibroadcast PUBLIC
 )
 
 ## FEC Optimizations begin ---------------------------------
-set(WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_X86 -mavx2 -faligned-new=256)
-set(WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_ARM -mfpu=neon -march=armv7-a -marm)
+set(WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_X86_SSSE3 -mssse3 -faligned-new=256)
+set(WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_ARM_NEON -mfpu=neon -march=armv7-a -marm)
 include(CheckCXXCompilerFlag)
-check_cxx_compiler_flag("-mavx2" COMPILER_SUPPORTS_MAVX2)
+check_cxx_compiler_flag("-mssse3" COMPILER_SUPPORTS_X86_SSSE3)
 check_cxx_compiler_flag("-mfpu=neon" COMPILER_SUPPORTS_NEON)
-# AVX2 if supported and option WB_ENABLE_SIMD_OPTIMIZATIONS is true
-if(COMPILER_SUPPORTS_MAVX2)
-    message(STATUS "Compiler supports AVX2")
+# SSSE3 if supported and option WB_ENABLE_SIMD_OPTIMIZATIONS is true
+if(COMPILER_SUPPORTS_X86_SSSE3)
+    message(STATUS "Compiler supports SSSE3")
     if(WB_ENABLE_SIMD_OPTIMIZATIONS)
-        message(STATUS "WB compile with AVX2")
-        target_compile_options(wifibroadcast PUBLIC ${WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_X86})
+        message(STATUS "WB compile with SSSE3")
+        target_compile_options(wifibroadcast PUBLIC ${WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_X86_SSSE3})
         # I do not know why, but using target_compile_options seems to be not enough ...
-        add_compile_options(${WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_X86})
-        target_compile_definitions(wifibroadcast PUBLIC WIFIBROADCAST_HAS_X86_AVX2)
+        add_compile_options(${WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_X86_SSSE3})
+        target_compile_definitions(wifibroadcast PUBLIC WIFIBROADCAST_HAS_X86_SSSE3)
     endif()
 endif()
 # NEON if supported and option WB_ENABLE_SIMD_OPTIMIZATIONS is true
@@ -56,9 +56,9 @@ if(COMPILER_SUPPORTS_NEON)
     message(STATUS "Compiler supports NEON")
     if(WB_ENABLE_SIMD_OPTIMIZATIONS)
         message(STATUS "WB compile with NEON")
-        target_compile_options(wifibroadcast PUBLIC ${WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_ARM})
+        target_compile_options(wifibroadcast PUBLIC ${WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_ARM_NEON})
         # I do not know why, but using target_compile_options seems to be not enough ...
-        add_compile_options(${WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_ARM})
+        add_compile_options(${WIFIBROADCAST_FEC_OPTIMIZATION_FLAGS_ARM_NEON})
         target_compile_definitions(wifibroadcast PUBLIC WIFIBROADCAST_HAS_ARM_NEON)
     endif()
 endif()
