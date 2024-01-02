c22c564d57eba1bc2d1b63f7c717a9b4158acd42
consti10 <geierconstantinabc@gmail.com>
2024-01-02 14:18:38 +0100

    fix - initial values of radiotap header are oerwritten on the first set_xxx call

--- src/radiotap/RadiotapHeaderTxHolder.hpp
+++ src/radiotap/RadiotapHeaderTxHolder.hpp
@@ -23,12 +23,8 @@ class RadiotapHeaderTxHolder {
     m_console=wifibroadcast::log::get_default();
   }
   void thread_safe_set(RadiotapHeaderTx::UserSelectableParams params){
-    auto tmp= RadiotapHeaderTx{params};
-    thread_safe_set2(tmp);
-  }
-  void thread_safe_set2(RadiotapHeaderTx radiotap_header) {
-    std::lock_guard<std::mutex> guard(m_radiotap_header_mutex);
-    m_radiotap_header = radiotap_header;
+    m_radioTapHeaderParams=params;
+    update_locked();
   }
   RadiotapHeaderTx thread_safe_get() {
     std::lock_guard<std::mutex> guard(m_radiotap_header_mutex);
@@ -38,12 +34,12 @@ class RadiotapHeaderTxHolder {
   void update_mcs_index(uint8_t mcs_index){
     m_console->debug("update_mcs_index {}",mcs_index);
     m_radioTapHeaderParams.mcs_index=mcs_index;
-    thread_safe_set(m_radioTapHeaderParams);
+    update_locked();
   }
   void update_channel_width(int width_mhz){
     m_console->debug("update_channel_width {}",width_mhz);
     m_radioTapHeaderParams.bandwidth=width_mhz;
-    thread_safe_set(m_radioTapHeaderParams);
+    update_locked();
   }
   void update_stbc(int stbc){
     m_console->debug("update_stbc {}",stbc);
@@ -52,19 +48,26 @@ class RadiotapHeaderTxHolder {
       return ;
     }
     m_radioTapHeaderParams.stbc=stbc;
-    thread_safe_set(m_radioTapHeaderParams);
+    update_locked();
   }
   void update_guard_interval(bool short_gi){
     m_radioTapHeaderParams.short_gi=short_gi;
-    thread_safe_set(m_radioTapHeaderParams);
+    update_locked();
   }
   void update_ldpc(bool ldpc){
     m_radioTapHeaderParams.ldpc=ldpc;
-    thread_safe_set(m_radioTapHeaderParams);
+    update_locked();
   }
   void update_set_flag_tx_no_ack(bool enable){
     m_radioTapHeaderParams.set_flag_tx_no_ack=enable;
-    thread_safe_set(m_radioTapHeaderParams);
+    update_locked();
+  }
+ private:
+  void update_locked(){
+    auto header= RadiotapHeaderTx{m_radioTapHeaderParams};
+    // Swap out the actual header (thread-safe)
+    std::lock_guard<std::mutex> guard(m_radiotap_header_mutex);
+    m_radiotap_header=header;
   }
  private:
   std::shared_ptr<spdlog::logger> m_console;
