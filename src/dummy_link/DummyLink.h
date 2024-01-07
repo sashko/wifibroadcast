b620a568236393566fcd6206d0eda026cb07c33f
consti10 <geierconstantinabc@gmail.com>
2024-01-07 19:55:14 +0100

    improve video fec

--- src/dummy_link/DummyLink.h
+++ src/dummy_link/DummyLink.h
@@ -7,6 +7,8 @@
 
 #include <cstdint>
 
+// TODO: Write something that emulates a wb link (tx, rx)
+// using linux shm or similar
 class DummyLink {
 public:
     void tx_radiotap(const uint8_t* packet_buff, int packet_size);
