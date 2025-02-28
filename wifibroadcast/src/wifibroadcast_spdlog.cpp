6b3a6950708630ea17a9261692a622904578be39
Raphael <raphael@openhdfpv.org>
2025-02-28 22:16:33 +0100

    Update wifibroadcast_spdlog.cpp

--- wifibroadcast/src/wifibroadcast_spdlog.cpp
+++ wifibroadcast/src/wifibroadcast_spdlog.cpp
@@ -1,13 +1,12 @@
 //
 // Created by consti10 on 13.08.23.
 //
-
 #include "wifibroadcast_spdlog.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
-
 #include <cassert>
 #include <mutex>
+#include <fstream>
 
 std::shared_ptr<spdlog::logger> wifibroadcast::log::create_or_get(
     const std::string& logger_name) {
@@ -16,11 +15,14 @@ std::shared_ptr<spdlog::logger> wifibroadcast::log::create_or_get(
   auto ret = spdlog::get(logger_name);
   if (ret == nullptr) {
     auto created = spdlog::stdout_color_mt(logger_name);
-    if (std::filesystem::exists("/usr/share/openhd/debug.txt")) {
-      created->set_level(spdlog::level::debug);
-    } else {
+
+    std::ifstream file("/usr/share/openhd/debug.txt");
+    if (file.good()) {
       created->set_level(spdlog::level::warn);
+    } else {
+      created->set_level(spdlog::level::debug);
     }
+
     assert(created);
     return created;
   }
